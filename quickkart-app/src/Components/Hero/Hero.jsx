ef65e89a4c6d4366cd167be86c21f544d4af811e
utkarsha3011 <utkarshatalele746@gmail.com>
2026-02-20 23:16:27 +0530

    add info

--- quickkart-app/src/Components/Hero/Hero.jsx
+++ quickkart-app/src/Components/Hero/Hero.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import model1 from '../Assets/model1.png'
+import arrow from '../Assets/arrow.png'
 const Hero = () => {
   return (
     <div className='hero'>
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
+        <h2>Discover Our</h2>
+        <h2>latest collections</h2>
         <div>
             <div className="hero-hand-icon">
                 <p>new</p>
                 <img src={hand_icon} alt="" height={100}/>
             </div>
-            <p>collections</p>
-            <p>for everyone</p>
+            <p><b>NEW ARRIVALS</b> FOR MEN, WOMEN & KIDS</p>
+            
         </div>
-        <div className="hero-latest-button">
+        <div className="hero-latest-btn">
             <div>Latest Collection</div>
-            {/* <img src={} alt="" /> */}
+            <img src={arrow} alt="" height={100} />
         </div>
       </div>
       <div className="hero-right">
