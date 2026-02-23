c2347a5f3dc6d671ea31e0cdf67f0faa52b3b2f0
utkarsha3011 <utkarshatalele746@gmail.com>
2026-02-23 17:29:46 +0530

    Main Homepage modification

--- quickkart-app/src/Components/Hero/Hero.jsx
+++ quickkart-app/src/Components/Hero/Hero.jsx
@@ -6,15 +6,18 @@ import arrow from '../Assets/arrow.png'
 const Hero = () => {
   return (
     <div className='hero'>
-      <div className="hero-left">
-        <h2>Discover Our</h2>
-        <h2>latest collections</h2>
-        <div>
+      <div className="hero-left mt-5">
+        <h2>Discover Our <br/> latest collections</h2>
+        
+        <div className=''>
             <div className="hero-hand-icon">
-                <p>new</p>
-                <img src={hand_icon} alt="" height={100}/>
+                <p className='mt-4'><b>NEW ARRIVALS</b>
+            <img src={hand_icon} alt=""/></p>
+             <p>FOR MEN, WOMEN & KIDS</p>
+                {/* <p>new</p> */}
+                {/* <img src={hand_icon} alt="" height={100}/> */}
             </div>
-            <p><b>NEW ARRIVALS</b> FOR MEN, WOMEN & KIDS</p>
+            
             
         </div>
         <div className="hero-latest-btn">
