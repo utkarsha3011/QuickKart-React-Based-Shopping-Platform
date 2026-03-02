dfd07f001783ba75f425b641d23f01b45401b407
utkarsha3011 <utkarshatalele746@gmail.com>
2026-03-02 11:00:43 +0530

    Add some Modification

--- quickkart-app/src/Components/Item/Item.jsx
+++ quickkart-app/src/Components/Item/Item.jsx
@@ -8,10 +8,10 @@ const Item = (props) => {
       <p>{props.name}</p>
       <div className="item prices">
         <span className="item-price-new">
-            ${props.new_price}
+            Rs.{props.new_price}
         </span> &nbsp; &nbsp;
         <span className="item-price-old">
-            ${props.old_price}
+            Rs.{props.old_price}
         </span>
       </div>
     </div>
